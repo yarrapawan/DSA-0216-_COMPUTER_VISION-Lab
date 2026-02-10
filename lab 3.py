a3f9e8c073ded8605e7f5786b01529938de87a90
yarrapawan <pawanyarra1611@gmail.com>
2026-02-10 14:20:35 +0530

    Delete lab 3.py

--- lab 3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-image = cv2.imread(r"C:\Users\pawan\OneDrive\Desktop\COPUTER VISION\Lab3 photo.jpeg") 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 100, 200)
-cv2.imshow("Original Image", image)
-cv2.imshow("Canny Edge Detection (Outline)", edges)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
