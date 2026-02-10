4934f332b2f7e3beca78613948cea2c11ed68be1
yarrapawan <pawanyarra1611@gmail.com>
2026-02-10 14:19:03 +0530

    Delete lab 1.py

--- lab 1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-image = cv2.imread(r"C:\Users\pawan\OneDrive\Desktop\COPUTER VISION\Lab1 photo.jpeg")
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Original Image", image)
-cv2.imshow("Grayscale Image", gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite("gray_image.jpg", gray_image)
