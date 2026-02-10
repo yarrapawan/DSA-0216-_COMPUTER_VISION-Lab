73aaf65b7cef12652338754a0c34d5959e7a3ab1
yarrapawan <pawanyarra1611@gmail.com>
2026-02-10 14:19:20 +0530

    Delete lab 2.py

--- lab 2.py
+++
@@ -1,7 +0,0 @@
-import cv2
-image = cv2.imread(r"C:\Users\pawan\OneDrive\Desktop\COPUTER VISION\Lab2 photo.jpeg")
-blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
-cv2.imshow("Original Image", image)
-cv2.imshow("Gaussian Blurred Image", blurred_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
